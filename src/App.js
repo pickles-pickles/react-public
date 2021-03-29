bd3075fefa62f85fe37610af95523d362a4c896d
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2021-03-29 15:18:47 +0300

    Personal site v.0.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello gh-pages</h1>
+      <h2>It's been a long way</h2>
+    </div>
+  );
+}
+
+export default App;
