2a4e8f48d335b371a881e2651eaab5fd7ef136b9
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-02-28 20:58:31 +0200

    lazy-loading-of-separate-components-removed.Now-only-the-Main-component-loads-lazily

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,10 @@ import React from 'react'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import NavigationBar from './NavigationBar'
 import Footer from './Footer'
-//import Home from './Home';
-/* import Portfolio from './Portfolio';
-import About from './About';
-import Contact from './Contact'; */
-const Home = React.lazy(() => import('./Home'))
-const Portfolio = React.lazy(() => import('./Portfolio'))
-const About = React.lazy(() => import('./About'))
-const Contact = React.lazy(() => import('./Contact'))
+import Home from './Home'
+import Portfolio from './Portfolio'
+import About from './About'
+import Contact from './Contact'
 
 const Main = () => {
   return (
