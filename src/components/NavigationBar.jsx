836878cad083cee28b4c5ee509c05b81e11dfc8f
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-02-28 22:44:57 +0200

    nav-links-ative-links-styling.FIXED:bug-in-nav-link-hover

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Nav, NavItem } from 'reactstrap'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
@@ -8,24 +8,45 @@ const NavigationBar = () => {
       <div className='container'>
         <Nav className='navbar'>
           <NavItem className='nav-item'>
-            <Link className='nav-link  col-sm-5 col-md-4' to='/'>
-              <span className='fa fa-home mr-1'></span>Home
-            </Link>
+            <NavLink
+              className='nav-link  col-sm-5 col-md-8'
+              activeClassName='nav-link-selected'
+              exact
+              to='/'
+            >
+              <span className='fa fa-home mr-1'></span>
+              <span className='nav-link-text'>Home</span>
+            </NavLink>
           </NavItem>
           <NavItem className='nav-item'>
-            <Link className='nav-link  col-sm-5 col-md-4' to='/portfolio'>
-              <span className='fa fa-address-card mr-1'></span>Portfolio
-            </Link>
+            <NavLink
+              className='nav-link  col-sm-5 col-md-8'
+              activeClassName='nav-link-selected'
+              to='/portfolio'
+            >
+              <span className='fa fa-address-card mr-1'></span>
+              <span className='nav-link-text'>Portfolio</span>
+            </NavLink>
           </NavItem>
           <NavItem className='nav-item'>
-            <Link className='nav-link  col-sm-5 col-md-4' to='/about'>
-              <span className='fa fa-user-alt mr-1'></span>About
-            </Link>
+            <NavLink
+              className='nav-link  col-sm-5 col-md-8'
+              activeClassName='nav-link-selected'
+              to='/about'
+            >
+              <span className='fa fa-user-alt mr-1'></span>
+              <span className='nav-link-text'>About</span>
+            </NavLink>
           </NavItem>
           <NavItem className='nav-item'>
-            <Link className='nav-link  col-sm-5 col-md-4' to='/contact'>
-              <span className='fa fa-telegram  mr-1'></span>Contact
-            </Link>
+            <NavLink
+              className='nav-link  col-sm-5 col-md-8'
+              activeClassName='nav-link-selected'
+              to='/contact'
+            >
+              <span className='fa fa-telegram  mr-1'></span>
+              <span className='nav-link-text'>Contact</span>
+            </NavLink>
           </NavItem>
         </Nav>
       </div>
