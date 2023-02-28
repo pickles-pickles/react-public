cbf65cafad8d7f67b00a00c43749b22ae84ee2d3
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-02-28 20:01:50 +0200

    refactor-to-match-components-names-with-their-content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import NavigationBar from './NavigationBar'
 import Footer from './Footer'
+//import Home from './Home';
+/* import Portfolio from './Portfolio';
+import About from './About';
+import Contact from './Contact'; */
+const Home = React.lazy(() => import('./Home'))
+const Portfolio = React.lazy(() => import('./Portfolio'))
+const About = React.lazy(() => import('./About'))
+const Contact = React.lazy(() => import('./Contact'))
 
 const Main = () => {
   return (
     <div className='animation-box'>
       <Router>
         <NavigationBar />
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route path='/portfolio' component={Portfolio}></Route>
+          <Route path='/about' component={About}></Route>
+          <Route path='/contact' component={Contact}></Route>
+        </Switch>
         <Footer />
       </Router>
     </div>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,53 +1,35 @@
 import React from 'react'
 import { Nav, NavItem } from 'reactstrap'
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
-//import Home from './Home';
-/* import Portfolio from './Portfolio';
-import About from './About';
-import Contact from './Contact'; */
-
-const Home = React.lazy(() => import('./Home'))
-const Portfolio = React.lazy(() => import('./Portfolio'))
-const About = React.lazy(() => import('./About'))
-const Contact = React.lazy(() => import('./Contact'))
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
     <div>
       <React.Suspense fallback={<h2>Component Loading ...</h2>}>
-        <Router>
-          <div className='container'>
-            <Nav className='navbar'>
-              <NavItem className='nav-item'>
-                <Link className='nav-link  col-sm-5 col-md-4' to='/'>
-                  <span className='fa fa-home mr-1'></span>Home
-                </Link>
-              </NavItem>
-              <NavItem className='nav-item'>
-                <Link className='nav-link  col-sm-5 col-md-4' to='/portfolio'>
-                  <span className='fa fa-address-card mr-1'></span>Portfolio
-                </Link>
-              </NavItem>
-              <NavItem className='nav-item'>
-                <Link className='nav-link  col-sm-5 col-md-4' to='/about'>
-                  <span className='fa fa-user-alt mr-1'></span>About
-                </Link>
-              </NavItem>
-              <NavItem className='nav-item'>
-                <Link className='nav-link  col-sm-5 col-md-4' to='/contact'>
-                  <span className='fa fa-telegram  mr-1'></span>Contact
-                </Link>
-              </NavItem>
-            </Nav>
-          </div>
-
-          <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route path='/portfolio' component={Portfolio}></Route>
-            <Route path='/about' component={About}></Route>
-            <Route path='/contact' component={Contact}></Route>
-          </Switch>
-        </Router>
+        <div className='container'>
+          <Nav className='navbar'>
+            <NavItem className='nav-item'>
+              <Link className='nav-link  col-sm-5 col-md-4' to='/'>
+                <span className='fa fa-home mr-1'></span>Home
+              </Link>
+            </NavItem>
+            <NavItem className='nav-item'>
+              <Link className='nav-link  col-sm-5 col-md-4' to='/portfolio'>
+                <span className='fa fa-address-card mr-1'></span>Portfolio
+              </Link>
+            </NavItem>
+            <NavItem className='nav-item'>
+              <Link className='nav-link  col-sm-5 col-md-4' to='/about'>
+                <span className='fa fa-user-alt mr-1'></span>About
+              </Link>
+            </NavItem>
+            <NavItem className='nav-item'>
+              <Link className='nav-link  col-sm-5 col-md-4' to='/contact'>
+                <span className='fa fa-telegram  mr-1'></span>Contact
+              </Link>
+            </NavItem>
+          </Nav>
+        </div>
       </React.Suspense>
     </div>
   )
