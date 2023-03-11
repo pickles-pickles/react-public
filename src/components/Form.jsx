c6b83b17c0812165c7ecec8ce680464c1d1baaaa
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-03-11 21:52:27 +0200

    FIXED:bug-in-firebase-refinement

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,7 +10,7 @@ const Form = () => {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    projectId: String(process.env.REACT_APP_FIREBASE_PROJECT_ID),
     storageBucket: process.env.REACT_APP_FIREBASE_API_KEY_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
