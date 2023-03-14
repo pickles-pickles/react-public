108383a7bb66ec737694631b862b4f8f7645023e
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-03-14 21:24:07 +0200

    updated-next-goals-card

--- src/components/cards/About/FutureGoalsCard.jsx
+++ src/components/cards/About/FutureGoalsCard.jsx
@@ -11,9 +11,21 @@ const FutureGoalsCard = () => {
         }}
       >
         <p>
-          I am working to expand my knowledge on my stack and achieve excellence
-          in depth and in width, regarding web development and Informatics in
-          general.
+          My main goals for 2023 are to:
+          <br />
+          <br />
+          <ul
+            style={{
+              marginLeft: '0px',
+              paddingLeft: '15px'
+            }}
+          >
+            <li>Enhance my online presence</li>
+            <li>Decide what my next steps will be, since I completed my MSc</li>
+          </ul>
+          Furthermore, I am working to expand my knowledge on my stack and
+          achieve excellence in depth and in width, regarding web development
+          and Informatics in general.
         </p>
         <p>
           {' '}
