d487a71591815697472e430cfe80cada92aa4b57
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-07-20 16:23:57 +0300

    more elements' color changes when the theme changes

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -2,36 +2,32 @@ import React, { useState, useEffect } from 'react'
 import Toggle from 'react-toggle'
 
 const ThemeToggler = () => {
-  const [isLight, setIsLight] = useState(true)
+  const [isLight, setIsLight] = useState(
+    window.matchMedia('(prefers-color-scheme: light)').matches
+  )
 
   useEffect(() => {
     if (isLight) {
       document.body.classList.remove('dark')
+      console.log(document.body.classList)
     } else {
       document.body.classList.add('dark')
+      console.log(document.body.classList)
     }
   }, [isLight])
 
-  useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme: light)')) {
-      console.log('light')
-      setIsLight(true)
-    } else if (window.matchMedia('(prefers-color-scheme: dark)')) {
-      console.log('dark')
-      setIsLight(false)
-    }
-  }, [])
-
   return (
-    <Toggle
-      checked={isLight}
-      onChange={({ target }) => {
-        setIsLight(target.checked)
-        console.log(target.checked)
-      }}
-      icons={{ checked: '🔆', unchecked: '🌙' }}
-      aria-label='Dark mode toggle'
-    />
+    <>
+      <Toggle
+        checked={isLight}
+        onChange={({ target }) => {
+          setIsLight(target.checked)
+          console.log('is checked', target.checked)
+        }}
+        icons={{ checked: '🔆', unchecked: '🌙' }}
+        aria-label='Dark mode toggle'
+      />
+    </>
   )
 }
 
