4160c8af461366f152518d09c07c99427ffdb68a
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-23 23:18:35 +0300

    improve layout markup for large screens

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,28 +27,27 @@ const ContainerDiv = styled.div`
   }
 `
 
-
 const Home = () => {
   return (
     <ContainerDiv>
-    <div className='containerd'>
-      <div className='row mt-3'>
-        <div
-          className='col-sm-12
+      <div className='containerd'>
+        <div className='row mt-3'>
+          <div
+            className='col-sm-12
                 col-md-8 offset-md-2
                   col-lg-6 offset-lg-0
                   order-lg-last
                   text-center slide-right-to-left-home-4'
-        >
-          <LazyLoadImage
-            src={ImageHome}
-            placeholdersrc={PlaceholderImageHome}
-            alt='Image Alt'
-            id='personal-image'
-            effect='blur'
-            wrapperProps={{
-              style: {
-                /* display: 'inline-block',
+          >
+            <LazyLoadImage
+              src={ImageHome}
+              placeholdersrc={PlaceholderImageHome}
+              alt='Image Alt'
+              id='personal-image'
+              effect='blur'
+              wrapperProps={{
+                style: {
+                  /* display: 'inline-block',
                 WebkitMaskImage:
                   MaskImage,
                 maskImage:
@@ -56,41 +55,42 @@ const Home = () => {
                   MaskImage,
                 maskRepeat: 'no-repeat',
                 maskSize: 'cover' */
-              }
-            }}
-          />
-          {/* <img src={ImageHome} /> */}
-        </div>
-        <div className='col-sm-12 text-center col-lg-6'>
-          <h1 id='title' className='slide-right-to-left-home mt-4 mt-lg-0'>
-            I am Chris
-          </h1>
-          <h2 id='subtitle' className='slide-left-to-right-home'>
-            React developer
-          </h2>
-          <h2 id='subtitle-smaller' className='slide-right-to-left-home-2'>
-            MSc in Software Engineering
-          </h2>
-          {/* <h2 id='subtitle-small' className='slide-left-to-right-home-2'>
-            Open to opportunities
+                }
+              }}
+            />
+            {/* <img src={ImageHome} /> */}
+          </div>
+          <div className='col-sm-12 text-center col-lg-6'>
+            <h1 id='title' className='slide-right-to-left-home mt-4 mt-lg-0'>
+              I am Chris
+            </h1>
+            <h2 id='subtitle' className='slide-left-to-right-home'>
+              React developer
+            </h2>
+            <h2 id='subtitle-smaller' className='slide-right-to-left-home-2'>
+              MSc in Software Engineering
+            </h2>
+            {/* <h2 id='subtitle-small' className='slide-left-to-right-home-2'>
+            Open to opportunities 
           </h2> */}
-
-          <p className='text slide-right-to-left-home-3' id='home-text'>
-            I love front-end development, as my work is very close to the final
-            user.{' '}
-            {/* Learn more
+            <div className='bg-warning1'>
+              <p className='text slide-right-to-left-home-3' id='home-text'>
+                I love front-end development, as my work is very close to the
+                final user.{' '}
+                {/* Learn more
             <Link to='/about'>
               <b> about me</b>
             </Link>{' '}
             or send me a message. */}
-          </p>
-          <div className='slide-right-to-left-home-4'>
-            <Form />
-            <MaskImage/>
+              </p>
+              <div className='slide-right-to-left-home-4'>
+                <Form />
+              </div>
+            </div>
+            <MaskImage />
           </div>
         </div>
       </div>
-    </div>
     </ContainerDiv>
   )
 }
