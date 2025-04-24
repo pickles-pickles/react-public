1ec32d49ba4b9914adced9c9423bdb79f938fb19
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-24 16:04:50 +0300

    experience section and title

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <section id='experience-section'>
+      <h1>EXPERIENCE</h1>
+      <div className='experience-content'></div>
+    </section>
+  )
+}
+
+export default Experience
--- src/pages/ScrollView.jsx
+++ src/pages/ScrollView.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Home from '../components/Home'
 import HomeTest from '../components/HomeTest'
 import MaskImage from '../components/MaskImage'
+import Experience from '../components/Experience'
 
 const ScrollView = () => {
   return (
     <>
       <Home />
+      <Experience />
       <MaskImage />
 
       {/* <HomeTest/> */}
