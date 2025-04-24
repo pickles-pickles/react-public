65ad7dde2f567c54ccd68181e142ee34d932c2e9
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-24 19:50:05 +0300

    hide footer

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -21,7 +21,7 @@ const Main = () => {
         <Route path='/about' component={About}></Route>
         <Route path='/contact' component={Contact}></Route>
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
