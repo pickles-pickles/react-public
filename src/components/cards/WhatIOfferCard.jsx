674701ebf37ae7d288c72c85193d44f708d507e4
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-30 15:08:15 +0300

    what can i offer card styling

--- src/components/cards/WhatIOfferCard.jsx
+++ src/components/cards/WhatIOfferCard.jsx
@@ -4,8 +4,10 @@ const WhatIOfferCard = () => {
   return (
     <div className=' glass-card'>
       {' '}
-      I can help you achieve your business goals offering:
-      <ul>
+      <h2 id='what-can-i-offer-card-title'>
+        Achieve your business goals with my:
+      </h2>
+      <ul className='experience-card-subtitle what-i-offer-card-list text-left pt-3'>
         <li>Front-expertise</li>
         <li>Efficient work as part of a team and indepedently</li>
         <li>Requirements Specification</li>
