5e7f2cfe3366c1c5500cb28e77c696d3118d9e5f
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-30 15:32:26 +0300

    stack list card styling

--- src/components/ListStack.jsx
+++ src/components/ListStack.jsx
@@ -2,21 +2,34 @@ import React from 'react'
 
 const ListStack = () => {
   return (
-    <div id='list-stck-container'>
-      My stack includes industry-standard technology like:
-      <ul>
-        <li>React</li>
-        <li>Redux</li>
-        <li>Bootstrap</li>
-        <li>Typescript</li>
-        <li>Material UI</li>
-        <li>Python</li>
-        <li>Docker</li>
-        <li>Echarts</li>
-        <li>CCS preprocessors</li>
-        <li>Node</li>
-        <li>Express</li>
-      </ul>
+    <div className='d-flex justify-content-center py-5'>
+      ;
+      <div
+        id='list-stck-container'
+        className='glass-card
+'
+      >
+        <h2 id='stack-list-card-title'>
+          My stack includes industry-standard technology like:
+        </h2>
+
+        <ul
+          className='experience-card-subtitle
+'
+        >
+          <li>React</li>
+          <li>Redux</li>
+          <li>Bootstrap</li>
+          <li>Typescript</li>
+          <li>Material UI</li>
+          <li>Python</li>
+          <li>Docker</li>
+          <li>Echarts</li>
+          <li>CCS preprocessors</li>
+          <li>Node</li>
+          <li>Express</li>
+        </ul>
+      </div>
     </div>
   )
 }
