0c30e21b0148b4675c625700bf21315dbacb7aa0
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-30 15:01:49 +0300

    what can i offer card

--- src/components/cards/WhatIOfferCard.jsx
+++ src/components/cards/WhatIOfferCard.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const WhatIOfferCard = () => {
-  return <div>WhatIOfferCard</div>
+  return (
+    <div className=' glass-card'>
+      {' '}
+      I can help you achieve your business goals offering:
+      <ul>
+        <li>Front-expertise</li>
+        <li>Efficient work as part of a team and indepedently</li>
+        <li>Requirements Specification</li>
+        <li>Documentation Writing</li>
+      </ul>
+    </div>
+  )
 }
 
 export default WhatIOfferCard
