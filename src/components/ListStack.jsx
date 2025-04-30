762e167633cf01b2b1fe7620ac6afa746e4b8730
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-30 14:56:13 +0300

    list stack

---
+++ src/components/ListStack.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ListStack = () => {
+  return (
+    <div id='list-stck-container'>
+      My stack includes industry-standard technology like:
+      <ul>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Bootstrap</li>
+        <li>Typescript</li>
+        <li>Material UI</li>
+        <li>Python</li>
+        <li>Docker</li>
+        <li>Echarts</li>
+        <li>CCS preprocessors</li>
+        <li>Node</li>
+        <li>Express</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ListStack
+
+/* const ContainerDiv = styled.section`` */
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,17 +3,19 @@ import SolarSystem from './3D/SolarSystem'
 import ISSModel from './3D/ISS'
 import SolarSystemStack from './3D/SolarSystemStack'
 import SpaceTelescopeStack from './3D/SpaceTelescopeStack'
+import ListStack from './ListStack'
 
 const Stack = () => {
   return (
     <section id='stack-section' style={{ background: 'black' }}>
       <h1 className='text-center'>My Stack</h1>
-      <div
+      <ListStack />
+      {/* <div
         style={{ height: '80vh', background: 'black', paddingBottom: '10px' }}
-      >
-        <SolarSystemStack />
-        {/* <SpaceTelescopeStack /> */}
-      </div>
+      > */}
+      {/* <SolarSystemStack /> */}
+      {/* <SpaceTelescopeStack /> */}
+      {/* </div> */}
       {/*       <div style={{ height: '60vh' }}>
         <ISSModel />
       </div> */}
