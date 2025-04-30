ac6225fc9da8ad91e50873e939b5321d4559d218
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2025-04-30 15:43:09 +0300

    fix home overfow x : it was due to margin in row class

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,47 +32,46 @@ const ContainerDiv = styled.section`
 
 const Home = () => {
   return (
-    <ContainerDiv
-      className='row mt-0 pt-4 pt-lg-5 pb-5 pb-md-0'
-      id='home-section'
-    >
-      <div
-        id='personal-image-div'
-        className='col-sm-12
+    <ContainerDiv className='mt-0 pt-4 pt-lg-5 pb-5 pb-md-0' id='home-section'>
+      <div className='row'>
+        <div
+          id='personal-image-div'
+          className='col-sm-12
                 col-md-8 offset-md-2
                   col-lg-6 offset-lg-0
                   order-lg-last
                   text-center slide-right-to-left-home-4'
-      >
-        <LazyLoadImage
-          src={ImageHome}
-          placeholdersrc={PlaceholderImageHome}
-          alt='Image Alt'
-          id='personal-image'
-          effect='blur'
-          wrapperProps={{
-            style: {}
-          }}
-        />
-      </div>
-      <div className='col-sm-12 text-center col-lg-6'>
-        <div className='col-lg-12'>
-          <h1 id='title' className='slide-right-to-left-home mt-4 mt-lg-0'>
-            I am Chris
-          </h1>
-          <h2 id='subtitle' className='slide-left-to-right-home'>
-            React developer
-          </h2>
-          <h2 id='subtitle-smaller' className='slide-right-to-left-home-2'>
-            MSc in Software Engineering
-          </h2>
-        </div>
-
-        <div
-          className='bg-warning1 d-flex justify-content-center align-items-center position-relative'
-          id='home-card-container'
         >
-          <HomeCard />
+          <LazyLoadImage
+            src={ImageHome}
+            placeholdersrc={PlaceholderImageHome}
+            alt='Image Alt'
+            id='personal-image'
+            effect='blur'
+            wrapperProps={{
+              style: {}
+            }}
+          />
+        </div>
+        <div className='col-sm-12 text-center col-lg-6'>
+          <div className='col-lg-12'>
+            <h1 id='title' className='slide-right-to-left-home mt-4 mt-lg-0'>
+              I am Chris
+            </h1>
+            <h2 id='subtitle' className='slide-left-to-right-home'>
+              React developer
+            </h2>
+            <h2 id='subtitle-smaller' className='slide-right-to-left-home-2'>
+              MSc in Software Engineering
+            </h2>
+          </div>
+
+          <div
+            className='bg-warning1 d-flex justify-content-center align-items-center position-relative'
+            id='home-card-container'
+          >
+            <HomeCard />
+          </div>
         </div>
       </div>
     </ContainerDiv>
