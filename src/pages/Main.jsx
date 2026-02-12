7f63309efb201e8e342627d245aecb44d1c3b072
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2026-02-12 20:34:26 +0200

    created ScrollView and updated Main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import NavigationBar from '../components/NavigationBar'
 import Footer from '../components/Footer'
-import Home from './Home'
-import Portfolio from './Portfolio'
-import About from './About'
-import Contact from './Contact'
+import ScrollView from './ScrollView'
 
 const Main = () => {
   return (
@@ -15,10 +12,7 @@ const Main = () => {
       </div>
 
       <Switch>
-        <Route exact path='/' component={Home}></Route>
-        <Route path='/portfolio' component={Portfolio}></Route>
-        <Route path='/about' component={About}></Route>
-        <Route path='/contact' component={Contact}></Route>
+        <Route exact path='/' component={ScrollView}></Route>
       </Switch>
       <Footer />
     </div>
---
+++ src/pages/ScrollView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './Home'
+
+const ScrollView = () => {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default ScrollView
