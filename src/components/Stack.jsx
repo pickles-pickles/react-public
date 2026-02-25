633f7bdc1bf03df357106d00dddcc0cecf45d4e0
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2026-02-25 17:18:02 +0200

    deleted code related to the 3D assets that I no longer use

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import SolarSystem from './3D/SolarSystem'
-import ISSModel from './3D/ISS'
-import SolarSystemStack from './3D/SolarSystemStack'
-import SpaceTelescopeStack from './3D/SpaceTelescopeStack'
 import ListStack from './ListStack'
 
 const Stack = () => {
@@ -10,15 +6,6 @@ const Stack = () => {
     <section id='stack-section' style={{ background: 'black' }}>
       <h1 className='text-center'>My Stack</h1>
       <ListStack />
-      {/* <div
-        style={{ height: '80vh', background: 'black', paddingBottom: '10px' }}
-      > */}
-      {/* <SolarSystemStack /> */}
-      {/* <SpaceTelescopeStack /> */}
-      {/* </div> */}
-      {/*       <div style={{ height: '60vh' }}>
-        <ISSModel />
-      </div> */}
     </section>
   )
 }
